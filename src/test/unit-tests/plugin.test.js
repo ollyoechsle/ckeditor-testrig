de651d04e6e4927cb33e4526469a7692bf7431a5
Olly Oechsle <owksley@gmail.com>
2015-08-25 12:11:28 +0100

    Adds tests for plugin interactions

--- src/test/unit-tests/plugin.test.js
+++ src/test/unit-tests/plugin.test.js
@@ -1,6 +1,6 @@
 describe('CKEditor Plugin', function () {
 
-    var editableElement, editorInstance;
+    var editableElement, editorInstance, widgetID = "counter1";
 
     beforeEach(function (startTests) {
 
@@ -13,19 +13,52 @@ describe('CKEditor Plugin', function () {
         CKEDITOR.plugins.addExternal('counter', '/base/src/main/webapp/ckeditor/plugins/counter/');
 
         editorInstance = CKEDITOR.replace(
-            editableElement = CKEDITOR_UTILS.getEditableElement("Here's my plugin: <div class='counter' data-count='42'></div>"),
+            editableElement = CKEDITOR_UTILS.getEditableElement("Here's my plugin: <div class='counter' data-count='42' id='" + widgetID + "'></div>"),
             options
         );
 
         /**
          * You cannot start tests until the editor instance returns an instance ready event
          */
-        editorInstance.on('instanceReady', startTests);
+        editorInstance.on('instanceReady', function () {
+            console.log("Instance ready");
+            startTests();
+        });
     });
 
-    it("Should be possible to see the widget's rendered markup", function (done) {
+    it("Should be possible to see the widget's rendered markup", function () {
 
         expect(editorInstance.getSnapshot()).toContain("[[42]]");
+
+    });
+
+    it("Should be registered properly", function () {
+        expect(editorInstance.widgets.registered["counter"]).toBeDefined();
+    });
+
+    it("Should be possible to access the widget instance and call methods on it", function () {
+
+        var widget = editorInstance.widgets.getByElement(editorInstance.document.getById(widgetID));
+
+        expect(widget).not.toBeNull();
+
+        widget._increment();
+
+        expect(editorInstance.getSnapshot()).toContain("[[43]]");
+
+    });
+
+    it("Should be possible to interact with the widget", function () {
+
+        var widget = editorInstance.widgets.getByElement(editorInstance.document.getById(widgetID));
+
+        expect(widget.element).toBeDefined();
+
+        expect(editorInstance.getSnapshot()).toContain("[[42]]");
+
+        widget.element.fire("mousemove");
+
+        expect(editorInstance.getSnapshot()).toContain("[[43]]");
 
     });
 
